98d1708bddd22b26592a1e1843831c5400522514
Luis Lavena <luislavena@gmail.com>
2018-03-30 20:52:17 -0400

    CI: Correctly tag latest image
    Also add some details to the output to help out debug.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,15 @@ echo Docker Hub login...
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # push image
+echo Pushing $IMAGE...
 docker push $IMAGE
 
 # tag and push if latest
 if [ "$LATEST" == "true" ]; then
+    echo Tagging latest with $IMAGE
     LATEST_IMAGE=${IMAGE/$VERSION/latest}
-    docker tag $LATEST_IMAGE $IMAGE
+    docker tag $IMAGE $LATEST_IMAGE
+
+    echo Pushing latest image...
     docker push $LATEST_IMAGE
 fi
